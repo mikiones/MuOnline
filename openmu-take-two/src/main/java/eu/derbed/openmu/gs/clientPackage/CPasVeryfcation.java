0515c3b5e5992d7fba53a8f75b0db67e718b0530
al <al@mini.notbed.com>
2013-12-21 22:08:57 +0200

    rewrite authentication process

--- openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CPasVeryfcation.java
+++ openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CPasVeryfcation.java
@@ -6,96 +6,81 @@ import static eu.derbed.openmu.gs.serverPackets.SLoginAuthAnsfer.PA_PassOK;
 import static eu.derbed.openmu.utils.UPacket.logTransfer;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import eu.derbed.openmu.database.LoadUser;
 import eu.derbed.openmu.gs.ClientThread;
 import eu.derbed.openmu.gs.database.MuDataBaseFactory;
 import eu.derbed.openmu.gs.muObjects.MuUser;
 import eu.derbed.openmu.gs.serverPackets.SLoginAuthAnsfer;
 import eu.derbed.openmu.utils.UString;
-
+import eu.derbed.util.ICallback;
 
 public class CPasVeryfcation extends ClientBasePacket {
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-
-	public CPasVeryfcation(byte[] decrypt, ClientThread client)
+	/**
+	 * @param decrypt
+	 * @param client
+	 * @throws IOException
+	 * @throws Throwable
+	 */
+	public CPasVeryfcation(byte[] decrypt, final ClientThread client)
 			throws IOException, Throwable {
 		super(decrypt);
-		_pas = "";
-		_use = "";
+
 		Dec3bit(2, 10);
 		Dec3bit(12, 10);
-		_use = readS(2, 10);
-		_pas = readS(12, 10);
-		log.debug("Authentication request received [user: '{}', password: '{}']", _use, _pas);
-//		AAA fix logic
-		boolean correctCredentials = doesUserNameExist(_use);
-		final byte response;
 
-		if (!correctCredentials) {
-			log.debug("User '{}' does not exist in the database!", _use);
-			response = PA_AccInvalt;
-		} else {
-			client.readUser(_use);
-			MuUser user = client.getUser();
+		logTransfer(log, _decrypt);
 
-			correctCredentials = user != null;
-			if (correctCredentials) {
-				String userName = UString.nvl(user.getUser());
-				String pass = UString.nvl(user.getPass());
+		final String username = readS(2, 10);
+		final String password = readS(12, 10);
 
-				logTransfer(log, _decrypt);
+		log.debug("Authentication request received [user: '{}', password: '{}']", username, password);
 
-				correctCredentials = StringUtils.equals(_use, userName) && StringUtils.equals(_pas, pass);
+		MuDataBaseFactory.execute(new LoadUser(username, new ICallback<MuUser>() {
+
+			@Override
+			public void resultArrived(MuUser user) throws Throwable {
+
+				final byte response;
+
+				if (user == null) {
+
+					log.debug("User '{}' does not exist in the database!", username);
+					response = PA_AccInvalt;
+
+				} else {
+
+					String dbUsername = UString.trimNvl(user.getUser());
+					String dbPassword = UString.trimNvl(user.getPass());
+
+					boolean correctCredentials = StringUtils.equals(username, dbUsername) && StringUtils.equals(password, dbPassword);
+
+					if (correctCredentials) {
+						log.debug("Logged in.");
+						response = PA_PassOK;
+						client.setUser(user);
+					} else {
+						log.debug("Wrong password.");
+						response = PA_InvaltPassword;
+					}
+
+				}
+
+				client.getConnection().sendPacket(new SLoginAuthAnsfer(response));
 
 			}
-
-			if (correctCredentials) {
-				log.debug("Logged in.");
-				response = PA_PassOK;
-			} else {
-				log.debug("Wrong password.");
-				response = PA_InvaltPassword;
-			}
-
-		}
-
-
-		client.getConnection().sendPacket(new SLoginAuthAnsfer(response));
+		}));
 	}
 
-	private String _pas;
-
-	private String _use;
-
+	/* (non-Javadoc)
+	 * @see eu.derbed.openmu.gs.clientPackage.ClientBasePacket#getType()
+	 */
 	@Override
 	public String getType() {
 		return "f101 authPack";
 	}
 
-	public boolean doesUserNameExist(String name) {
-		boolean result = false;
-
-
-		try (Connection con = MuDataBaseFactory.getInstance().getConnection()) {
-			final PreparedStatement statement = con
-					.prepareStatement("SELECT * FROM users where u_user=?");
-			// ("select * from users where u_login=?");
-			statement.setString(1, name);
-			final ResultSet rset = statement.executeQuery();
-			result = rset.next();
-			con.close();
-		} catch (final SQLException e) {
-			log.error("Failed to check if user exists.", e);
-		}
-		return result;
-	}
 }
