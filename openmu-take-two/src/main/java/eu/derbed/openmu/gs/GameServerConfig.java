9678c17a49f47b81a3ca33620dcc28dc964b6228
al <al@mini.notbed.com>
2014-03-03 20:22:53 +0200

    change loading

--- openmu-take-two/src/main/java/eu/derbed/openmu/gs/GameServerConfig.java
+++ openmu-take-two/src/main/java/eu/derbed/openmu/gs/GameServerConfig.java
@@ -2,6 +2,7 @@ package eu.derbed.openmu.gs;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,10 @@ import org.slf4j.LoggerFactory;
  * @author Marcel
  */
 public final class GameServerConfig {
-	private static GameServerConfig _instance;
+
+	private static final String GLOBAL_CONF = "global.conf";
+
+	private static GameServerConfig INSTANCE;
 	public static final Properties global = new Properties();
 	public static final Properties databse = new Properties();
 	public static final Properties gs = new Properties();
@@ -21,33 +25,57 @@ public final class GameServerConfig {
 	public final boolean testMode;
 
 	public static GameServerConfig getInstance() throws IOException {
-		if (_instance == null) {
-			_instance = new GameServerConfig();
+		if (INSTANCE == null) {
+			INSTANCE = new GameServerConfig();
 		}
-		return _instance;
+		return INSTANCE;
 	}
 
-	private GameServerConfig() throws IOException {
-		//global.put("global.home", System.getProperty("user.home")+"/.jmuserv/");
+	/**
+	 *
+	 */
+	private GameServerConfig() {
 		global.put("global.home", System.getProperty("user.dir"));
-		global.put("global.conf", global.getProperty("global.home") + "/src/main/resources");
-		global.put("global.itemFile", global.getProperty("global.conf") + "/data/item.txt");
-		global.put("global.mapsDir", global.getProperty("global.conf") + "/data/maps/");
+		global.put(GLOBAL_CONF, global.getProperty("global.home") + "/src/main/resources");
+
+		final String conf = getConf();
+		global.put("global.itemFile", conf + "/data/item.txt");
+		global.put("global.mapsDir", conf + "/data/maps/");
+
 		loadConfig();
 
-		testMode = new Boolean(gs.getProperty("testmode", "false"));
+		testMode = Boolean.valueOf(gs.getProperty("testmode", "false"));
 	}
 
-	private void loadConfig() throws IOException {
-		databse.load(new FileInputStream(global.getProperty("global.conf") + "/conf/database.ini"));
-		gs.load(new FileInputStream(global.getProperty("global.conf") + "/conf/gameserver.ini"));
-		logs.load(new FileInputStream(global.getProperty("global.conf") + "/conf/mulog.ini"));
-		cs.load(new FileInputStream(global.getProperty("global.conf") + "/conf/connectserver.ini"));
+	/**
+	 * @return
+	 */
+	private String getConf() {
+		return global.getProperty(GLOBAL_CONF);
 	}
 
-	public void findDataDirectory() {
-		final String home = System.getProperty("user.dir");
-		System.out.println(home);
+	/**
+	 *
+	 */
+	private void loadConfig() {
+		final String conf = getConf();
+		load(conf, "/conf/database.ini", databse);
+		load(conf, "/conf/gameserver.ini", gs);
+		load(conf, "/conf/mulog.ini", logs);
+		load(conf, "/conf/connectserver.ini", cs);
+	}
+
+	/**
+	 * @param conf
+	 * @param file
+	 * @param dest
+	 */
+	private void load(String conf, String file, Properties dest) {
+		try (InputStream in = new FileInputStream(conf + file)) {
+			dest.load(in);
+		} catch (IOException e) {
+			throw new IllegalStateException("Failed to load " + file);
+		}
 	}
 
 	/**
