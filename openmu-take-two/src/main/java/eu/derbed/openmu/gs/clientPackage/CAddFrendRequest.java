e7920a6518478498fea054f54e8b8df29a9f8e09
Alexandru Bledea <admin@notbed.com>
2014-07-28 19:19:27 +0300

    convert to reusable object

--- openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CAddFrendRequest.java
+++ openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CAddFrendRequest.java
@@ -5,16 +5,21 @@
 
 package eu.derbed.openmu.gs.clientPackage;
 
+import com.notbed.muonline.util.DataDecrypter;
+
 import eu.derbed.openmu.gs.ClientThread;
 
 /**
  * 
  * @author Miki i Linka
  */
-public class CAddFrendRequest extends ClientBasePacket {
+public class CAddFrendRequest extends SimpleClientPackage {
 
-	public CAddFrendRequest(byte[] _decrypt, ClientThread _client) {
-		super(_decrypt);
+	/* (non-Javadoc)
+	 * @see eu.derbed.openmu.gs.clientPackage.SimpleClientPackage#process(com.notbed.muonline.util.DataDecrypter, eu.derbed.openmu.gs.ClientThread)
+	 */
+	@Override
+	protected void process(DataDecrypter decrypter, ClientThread client) {
 		System.out.println("|-Add Friend Request:");
 		String _name = decrypter.readS(2, 10);
 		_name = _name.trim();
