3c4bcf95147345cbcb82c84800d81813bd015cc9
Alexandru Bledea <admin@notbed.com>
2014-07-30 06:14:24 +0300

    use logger

--- openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CSelectCharacterOrExitRequest.java
+++ openmu-take-two/src/main/java/eu/derbed/openmu/gs/clientPackage/CSelectCharacterOrExitRequest.java
@@ -1,10 +1,15 @@
 package eu.derbed.openmu.gs.clientPackage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.notbed.muonline.util.DataDecrypter;
 
 import eu.derbed.openmu.gs.ClientThread;
 
 public class CSelectCharacterOrExitRequest extends SimpleClientPackage {
+
+	private static final Logger log = LoggerFactory.getLogger(CSelectCharacterOrExitRequest.class);
 
 	/* (non-Javadoc)
 	 * @see eu.derbed.openmu.gs.clientPackage.SimpleClientPackage#process(com.notbed.muonline.util.DataDecrypter, eu.derbed.openmu.gs.ClientThread)
@@ -13,13 +18,13 @@ public class CSelectCharacterOrExitRequest extends SimpleClientPackage {
 	protected void process(DataDecrypter dataDecrypter, ClientThread client) {
 		switch (dataDecrypter.data[2]) {
 			case 0x00:
-				System.out.println("exit game request");
+				log.debug("exit game request");
 				break;
 			case 0x01:
-				System.out.println("Change server request");
+				log.debug("Change server request");
 				break;
 			case 0x02:
-				System.out.println("Change character request");
+				log.debug("Change character request");
 				break;
 			}
 
